1529d6728ee01e81440601d82806462a7f633ede
Govind Krishna Joshi <govind@nilenso.com>
2021-11-12 18:27:11 +0530

    Add a subscribe directive as a test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,33 @@
+import { printSchemaWithDirectives } from "@graphql-tools/utils";
 import { ApolloServer } from "apollo-server";
+import { GraphQLSchema, lexicographicSortSchema } from "graphql";
 import path from "path";
 import "reflect-metadata";
 import "reflect-metadata";
 import { buildSchema } from "type-graphql";
 import UserResolver from "./resolvers/UserResolver";
 import WorkspaceResolver from "./resolvers/WorkSpaceResolver";
+import { outputFile } from "type-graphql/dist/helpers/filesystem";
 
 const port = process.env.PORT;
+
+export async function emitSchemaDefinitionWithDirectivesFile(
+  schemaFilePath: string,
+  schema: GraphQLSchema
+): Promise<void> {
+  const schemaFileContent = printSchemaWithDirectives(
+    lexicographicSortSchema(schema)
+  );
+  await outputFile(schemaFilePath, schemaFileContent);
+}
 
 const init = async () => {
   const schema = await buildSchema({
     resolvers: [UserResolver, WorkspaceResolver],
     emitSchemaFile: path.resolve(__dirname, "schema.graphql"),
   });
+
+  await emitSchemaDefinitionWithDirectivesFile("schema.gql", schema);
 
   const server = new ApolloServer({
     schema,
